c45d867187fc5adacaef875baac9e885edcc5c2c
Bhavna Haritsa <bhavnaharitsa@gmail.com>
2019-10-01 20:56:45 +0530

    intro page created

---
+++ app/src/main/java/com/example/quizapp/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.example.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.quizapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
